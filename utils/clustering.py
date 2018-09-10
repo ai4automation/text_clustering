bc0af846e7cb4431ca13051f8705d03c5e4b4dd1
root <root@process-min.sl.cloud9.ibm.com>
2018-09-10 11:11:49 +0000

    first phase

--- utils/clustering.py
+++ utils/clustering.py
@@ -45,5 +45,6 @@ def ranking(M, documents):
         ranking[seq] = pf * (-math.log(1 - df))
 
     sequence_sorted = sorted(ranking, key=ranking.get, reverse=True)[:50]
-    final_phrases = filter(lambda x: [x for i in sequence_sorted if x in i and x != i] == [], sequence_sorted)
+    overlap_phrases = [x for i in sequence_sorted if len(list(set(x.split(' ')) & set(i.split(' '))))>1 and x!=i]
+    #final_phrases = filter(lambda x: [x for i in sequence_sorted if x in i and x != i] == [], sequence_sorted)
     return final_phrases
