9055b41aee455e7ba2e8d83bdff92c82c8bec94f
Tanmayee <tanmayee@Tanmayees-MacBook-Pro.local>
2018-09-11 16:44:15 +0530

    added ngrams generation

--- utils/preprocess.py
+++ utils/preprocess.py
@@ -1,5 +1,6 @@
 import spacy
 import re
+from nltk import ngrams
 
 
 def clean_html(text):
@@ -8,21 +9,20 @@ def clean_html(text):
     return text
 
 
-def pre_process(text):
-    """
-    Pre-process text according to Key2Vec paper
-    :param text: string object to pre-process
-    :return: list of preprocessed sentences
-    """
-    text = clean_html(text)
+def clean_punctuation(text):
+    text = text.replace(' - ', '-')
+    text = text.replace(' / ', '/')
+    return text
+
+
+def remove_unwanted_pos(text, nlp):
+    doc = nlp(text)
 
     ENT_TYPES = ['DATE', 'TIME', 'PERCENT', 'MONEY', 'QUANTITY', 'ORDINAL', 'CARDINAL']
     POS_TAGS = ['INTJ', 'AUX', 'CCONJ', 'ADP', 'DET', 'NUM', 'PART', 'PRON', 'SCONJ', 'SYM', 'X']
     ALLOWED_PUNCTUATION = ['/', '-']
 
     output_sentences = []
-    nlp = spacy.load('en')
-    doc = nlp(text)
 
     for named_entity in list(doc.ents):
         named_entity.merge(tag=named_entity.root.tag_, lemma=named_entity.root.lemma_,
@@ -50,21 +50,73 @@ def pre_process(text):
             if len(token.text.split()) > 1:
                 small_doc = nlp(token.text)
                 start, end = 0, len(small_doc)
-                first_token, last_token = list(small_doc)[0], list(small_doc)[-1]
-                if first_token.pos_ in POS_TAGS or first_token.is_stop or first_token.tag_ in POS_TAGS \
-                        or first_token.is_punct:
-                    start = 1
-                if last_token.pos_ in POS_TAGS or last_token.is_stop or last_token.tag_ in POS_TAGS \
-                        or last_token.is_punct:
-                    end = -1
+
+                for i in range(len(small_doc)):
+                    first_token = list(small_doc)[i]
+                    if first_token.pos_ in POS_TAGS or first_token.is_stop or first_token.tag_ in POS_TAGS \
+                            or first_token.is_punct:
+                        start = i+1
+                    else:
+                        break
+
+                for j in range(len(small_doc)-1, -1, -1):
+                    last_token = list(small_doc)[j]
+                    if last_token.pos_ in POS_TAGS or last_token.is_stop or last_token.tag_ in POS_TAGS \
+                            or last_token.is_punct:
+                        end = j-1
+                    else:
+                        break
+
                 small_docs_token_lemma = []
+
                 for t in small_doc[start:end]:
-                    if not t.is_punct:
-                        small_docs_token_lemma.append(t.text)
-                output_tokens.append(' '.join(small_docs_token_lemma))
+                    if t.is_punct and t.text not in ALLOWED_PUNCTUATION:
+                        continue
+                    else:
+                        small_docs_token_lemma.append(t)
+                output_tokens.extend(small_docs_token_lemma)
             else:
-                output_tokens.append(token.lemma_)
+                output_tokens.append(token)
         output_sentences.append(output_tokens)
 
-    output_sentences = [' '.join(sentence_tokens) for sentence_tokens in output_sentences]
     return output_sentences
+
+
+def get_n_grams(list_of_texts, n=3):
+    POS_TAGS = ['INTJ', 'AUX', 'CCONJ', 'ADP', 'DET', 'NUM', 'PART', 'PRON', 'SCONJ', 'SYM', 'X']
+
+    nlp = spacy.load('en')
+
+    list_of_texts = list(set(list_of_texts))
+    list_of_texts = [clean_html(text) for text in list_of_texts]
+
+    list_of_texts = [remove_unwanted_pos(text, nlp) for text in list_of_texts]
+
+    all_n_grams = []
+    for text in list_of_texts:
+        text_n_grams = []
+        for sentence in text:
+            n_grams = ngrams(sentence, n)
+            text_n_grams.extend(n_grams)
+        all_n_grams.append(text_n_grams)
+
+    output_phrases = []
+
+    for text in all_n_grams:
+        one_text_ngrams = []
+        for one_ngram in text:
+            if one_ngram[0].pos_ in POS_TAGS or one_ngram[0].tag_ in POS_TAGS:
+                pass
+            elif one_ngram[-1].pos_ in POS_TAGS or one_ngram[-1].tag_ in POS_TAGS:
+                pass
+            elif sum([token.pos_ == 'VERB' for token in one_ngram]) > 0:
+                one_text_ngrams.append(' '.join([ng.lemma_ for ng in one_ngram]))
+            elif len(list(one_ngram[0].doc.ents)) > 0:
+                one_text_ngrams.append(' '.join([ng.lemma_ for ng in one_ngram]))
+            elif len(list(one_ngram[0].doc.noun_chunks)) > 0:
+                one_text_ngrams.append(' '.join([ng.lemma_ for ng in one_ngram]))
+            else:
+                pass
+        output_phrases.append(one_text_ngrams)
+
+    return output_phrases
