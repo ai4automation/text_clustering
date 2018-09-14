6698d056a65619f4a58c428dac2c74cb175ebb20
Tanmayee <tanarend@in.ibm.com>
2018-09-14 15:46:35 +0530

    added sub phrase logic

--- utils/clustering.py
+++ utils/clustering.py
@@ -26,6 +26,7 @@ import math
 
     return output_phrases'''
 
+
 def document_phrase_frequency(phrase, documents, mapping):
     df = 0
     pf = 0
@@ -38,7 +39,8 @@ def document_phrase_frequency(phrase, documents, mapping):
     df = df / float(len(documents))
     return df, pf
 
-def ranking(M, documents,mapping):
+
+def ranking(M, documents, mapping):
     ranking = {}
     for seq in M:
         df, pf = document_phrase_frequency(seq, documents, mapping)
@@ -47,34 +49,45 @@ def ranking(M, documents,mapping):
     sequence_sorted = sorted(ranking, key=ranking.get, reverse=True)[:50]
     return sequence_sorted, ranking
 
+
+def longest_sub_phrase(phrase_1, phrase_2):
+    phrase_1, phrase_2 = phrase_1.split(), phrase_2.split()
+    assert len(phrase_1) == len(phrase_2)
+    n = len(phrase_1)
+    for sub_phrase in [phrase_1[i:i + (n - 1)] for i in range(0, 2)]:
+        if sub_phrase in [phrase_2[i:i + (n - 1)] for i in range(0, 2)]:
+            return ' '.join(sub_phrase)
+    return None
+
+
 def post_process(ranked_list, M, documents, mapping, n=3):
-	final_list = []
-	merge = {}
-	for phrase in ranked_list:
-		flag = 0
-		for phrase_2 in final_list:
-			intersect_phrase = list(set(phrase.split(' ')) & set(phrase_2.split(' ')))
-			if len(intersect_phrase)==n-1:
-				#print(intersect_phrase)
-				if ' '.join(intersect_phrase) not in merge:
-					merge[' '.join(intersect_phrase)] = [phrase, phrase_2]
-				else:
-					list_merge = merge[' '.join(intersect_phrase)]
-					list_merge.append(phrase)
-					list_merge.append(phrase_2)
-					merge[' '.join(intersect_phrase)] = list_merge
-				flag = 1
-				#break
-		if flag==0:
-			final_list.append(phrase)
-	print(merge)
-	for phrase in merge.keys():
-		score_children = []
-		for children in merge[phrase]:
-			score_children.append(M[children])
-		df, pf = document_phrase_frequency(phrase, documents, mapping)
-		score_parent = pf * (-math.log(1 - df))
-		if score_parent > max(score_children):
-			final_list.append(phrase)
-			final_list = list(set(final_list)-set(merge[phrase]))
-	return final_list
+    final_list = []
+    merge = {}
+    for phrase in ranked_list:
+        flag = 0
+        for phrase_2 in final_list:
+            intersect_phrase = list(set(phrase.split(' ')) & set(phrase_2.split(' ')))
+            if len(intersect_phrase) == n - 1:
+                # print(intersect_phrase)
+                if ' '.join(intersect_phrase) not in merge:
+                    merge[' '.join(intersect_phrase)] = [phrase, phrase_2]
+                else:
+                    list_merge = merge[' '.join(intersect_phrase)]
+                    list_merge.append(phrase)
+                    list_merge.append(phrase_2)
+                    merge[' '.join(intersect_phrase)] = list_merge
+                flag = 1
+                # break
+        if flag == 0:
+            final_list.append(phrase)
+    print(merge)
+    for phrase in merge.keys():
+        score_children = []
+        for children in merge[phrase]:
+            score_children.append(M[children])
+        df, pf = document_phrase_frequency(phrase, documents, mapping)
+        score_parent = pf * (-math.log(1 - df))
+        if score_parent > max(score_children):
+            final_list.append(phrase)
+            final_list = list(set(final_list) - set(merge[phrase]))
+    return final_list
